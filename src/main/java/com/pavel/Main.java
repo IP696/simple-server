0dac4fbbb7b5af80bd2538f5e611c1836f109fbf
ppetrashov <ppetrashov@healthgorilla.com>
2020-02-25 15:03:32 +0300

    add bootstrap and first page from template

--- src/main/java/com/pavel/Main.java
+++ src/main/java/com/pavel/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/index", new MainHandler());
-        server.createContext("/", new ResourcesHandler());
+        server.createContext("/static", new ResourcesHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
